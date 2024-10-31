0696d64de6256c35b0ae2507e9d1f25e56a04516
andersasplundberggren <anders.asplundberggren@gmail.com>
2024-10-31 10:42:02 +0100

    Update chat_with_document.py

--- pages/chat_with_document.py
+++ pages/chat_with_document.py
@@ -8,7 +8,6 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.node_parser import SentenceSplitter
 from PIL import Image
 from uuid import uuid4
-
 import os
 from os import environ
 import hmac
@@ -18,7 +17,7 @@ from functions.menu import menu
 
 ### CSS AND STYLING
 
-st.logo("images/logo_main.png", icon_image = "images/logo_small.png")
+st.logo("images/logo_main.png", icon_image="images/logo_small.png")
 
 page_config()
 styling()
@@ -34,10 +33,9 @@ with st.expander("Välkommen"):
     st.write("Vi finns här för att assistera dig med dina frågor och ge de bästa svaren.")
     st.image("images/background.png", caption="Alltid redo att hjälpa dig!")
 
-
-# Check if language is already in session_state, else initialize it with a default value
+# Kontrollera om språket redan finns i session_state, annars initiera det med ett standardvärde
 if 'language' not in st.session_state:
-    st.session_state['language'] = "Svenska"  # Default language
+    st.session_state['language'] = "Svenska"  # Standard språk
 
 st.session_state["pwd_on"] = st.secrets.pwd_on
 
@@ -53,10 +51,9 @@ if st.session_state["pwd_on"] == "true":
             passwd = environ.get("password")
 
         def password_entered():
-
             if hmac.compare_digest(st.session_state["password"], passwd):
                 st.session_state["password_correct"] = True
-                del st.session_state["password"]  # Don't store the password.
+                del st.session_state["password"]  # Spara inte lösenordet.
             else:
                 st.session_state["password_correct"] = False
 
@@ -68,24 +65,21 @@ if st.session_state["pwd_on"] == "true":
             st.error("😕 Ooops. Fel lösenord.")
         return False
 
-
     if not check_password():
         st.stop()
-    
+
 ############
 
-os.makedirs("data", exist_ok=True) # Where data such as documents are beeing stored
-
+os.makedirs("data", exist_ok=True)  # Var data som dokument lagras
 
 if c.deployment == "streamlit":
-    llm = OpenAI(api_key = st.secrets.openai_key)
+    llm = OpenAI(api_key=st.secrets.openai_key)
     os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
 else:
-    llm = OpenAI(api_key = environ.get("openai_key"))
+    llm = OpenAI(api_key=environ.get("openai_key"))
     os.environ["OPENAI_API_KEY"] = environ.get("openai_key")
 
-
-# Translation
+# Översättning
 if st.session_state['language'] == "Svenska":
     prompt = """Du är en hjälpsam AI-assistent som hjälper användaren med sina frågor gällande den kontext du fått. Kontexten är ett eller flera dokument. 
 Basera alla dina svar på kontexten och hitta inte på något. Hjälp användaren svara på frågor, summera och annat. 
@@ -122,38 +116,33 @@ If you don't know the answer, respond that you don't know the answer.
     chat_input_text = "Your question?"
     thinking_text = "I'm thinking... One moment..."
 
-
 if 'system_prompt' not in st.session_state:
     st.session_state.system_prompt = prompt
 
 if "llm_temperature" not in st.session_state:
     st.session_state.llm_temperature = 0.2
 
-
-# Ensure each user has a unique directory
+# Se till att varje användare har en unik katalog
 if 'session_id' not in st.session_state:
-    st.session_state['session_id'] = str(uuid4())  # Generate a unique ID for each session
+    st.session_state['session_id'] = str(uuid4())  # Generera ett unikt ID för varje session
 
-# Create a user-specific data folder
+# Skapa en användarspecifik data-mapp
 user_data_folder = f'./data/{st.session_state["session_id"]}'
 os.makedirs(user_data_folder, exist_ok=True)
 
-
 Settings.llm = OpenAI(
     model="gpt-4o", 
-    temperature = st.session_state.llm_temperature,
-    system_prompt = st.session_state.system_prompt
-    )
+    temperature=st.session_state.llm_temperature,
+    system_prompt=st.session_state.system_prompt
+)
 
 Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
 embed_model = Settings.embed_model
 Settings.node_parser = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
 
-
 ### SIDEBAR
 
 menu()
-
 
 ### MAIN PAGE
 
@@ -163,32 +152,31 @@ with col1:
         
     if st.button(f"{clear_memory}"):
         
-        # Delete all files in the 'data' folder
+        # Ta bort alla filer i 'data' mappen
         folder_path = user_data_folder
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)  # Remove the file
+                    os.unlink(file_path)  # Ta bort filen
                 elif os.path.isdir(file_path):
-                    os.rmdir(file_path)  # Remove the folder if necessary
+                    os.rmdir(file_path)  # Ta bort mappen om det behövs
             except Exception as e:
                 st.error(f"Fel när filen {file_path} skulle tas bort: {e}")
 
-        # Clears all st.cache_resource caches:
+        # Rensa alla st.cache_resource-cachar:
         st.cache_resource.clear()
         st.success(f"{cache_cleared}")
 
-            
 with col2:
     with st.expander(f"{settings_text}"):
 
         llm_temp = st.slider(
             f'{temperature_text}',
-            min_value = 0.0,
-            max_value = 1.0,
-            step = 0.1,
-            value = 0.1,
+            min_value=0.0,
+            max_value=1.0,
+            step=0.1,
+            value=0.1,
         )
 
         st.markdown("###### ")
@@ -198,104 +186,46 @@ with col2:
             st.session_state.system_prompt = prompt_input   
             st.form_submit_button(f'{save_text}') 
 
-
 st.markdown(f"#### :material/description: {page_name}")
 
 @st.cache_resource(show_spinner=False)
 def load_data(user_data_folder):
-
-    with st.spinner(text=f"{loading_doc_text}"):
-
-        data = SimpleDirectoryReader(input_dir=user_data_folder, recursive=True).load_data()
-                
-        index = VectorStoreIndex.from_documents(
-            data, 
-            llm=llm,
-            embed_model=embed_model,
-            show_progress=True)
-        
-        return index    
-
+    try:
+        with st.spinner(text=f"{loading_doc_text}"):
+            data = SimpleDirectoryReader(input_dir=user_data_folder, recursive=True).load_data()
+            index = VectorStoreIndex.from_documents(
+                data, 
+                llm=llm,
+                embed_model=embed_model,
+                show_progress=True)
+            return index    
+    except Exception as e:
+        st.error(f"Ett fel inträffade när dokumentet skulle laddas: {e}")
+        return None
 
 uploaded_files = st.file_uploader(
     f"{upload_file_text}", 
     type=("pdf", "docx", "doc", "xls", "xlsx", "csv"), 
     accept_multiple_files=True)
 
-if uploaded_files:  # If files were uploaded
-
-    # Iterate through each uploaded file
+if uploaded_files:  # Om filer laddades upp
     for uploaded_file in uploaded_files:
-
         file_path = f"{user_data_folder}/{uploaded_file.name}"
 
-        # Check if the new file is different from the already indexed files
+        # Kontrollera om den nya filen är annorlunda än de redan indexerade filerna
         if 'indexed_file_paths' not in st.session_state:
             st.session_state.indexed_file_paths = []
 
         if file_path not in st.session_state.indexed_file_paths:
             st.session_state.indexed_file_paths.append(file_path)
 
-        # Save the file to disk
+        # Spara filen på disk
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getvalue())
 
-
 elif 'indexed_file_paths' in st.session_state:
-    # Reload existing files from the session state
-    #index = load_data()
-    pass
-
+    pass  # Ladda om befintliga filer från sessionens tillstånd om nödvändigt
 
 if "messages" not in st.session_state.keys(): 
     st.session_state.messages = [
-        {"role": "assistant", "content": f'''
-            {assistant_hello}
-        '''}
-    ]
-    st.session_state["messages"] = [{"role": "assistant", "content": f"{assistant_hello}"}]
-
-
-if uploaded_files:
-    index = load_data(user_data_folder)
-
-
-    query_engine = index.as_query_engine(
-        chat_mode = "openai", # openai context react
-        streaming = True,
-        similarity_top_k = 20,
-        verbose = True)
-
-
-    if prompt := st.chat_input(f"{chat_input_text}"): # Prompt for user input and save to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            # Check if the content is an image URL
-            if message["content"].startswith("http"):
-                st.image(message["content"])
-            else:
-                st.markdown(message["content"])
-
-
-    # If last message is not from assistant, generate a new response
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            
-            full_response = ""
-            message_placeholder = st.empty()
-
-            with st.spinner(f"{thinking_text}"):
-                streaming_response = query_engine.query(prompt)
-                
-                for response in streaming_response.response_gen:
-                    full_response += response
-                    message_placeholder.markdown(full_response + "▌")
-                
-                message = {"role": "assistant", "content": full_response}
-        
-            message_placeholder.markdown(full_response)
-            
-        st.session_state.messages.append(message) # Add response to message history
+        {"role": "
