e98e750cb8b5018e433338a3fffc9908b087a808
andersasplundberggren <anders.asplundberggren@gmail.com>
2024-11-07 10:55:03 +0100

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -1,23 +1,29 @@
-
-# External imports
 import streamlit as st
+from PIL import Image
+from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 from llama_index.core.llms import ChatMessage
-from openai import OpenAI
-from groq import Groq
-
-# Python imports
-import hmac
+from llama_index.core import Settings
+from llama_index.core import PromptTemplate
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.node_parser import SentenceSplitter
+from uuid import uuid4
 import os
 from os import environ
-
-# Local imports 
+import hmac
 from functions.styling import page_config, styling
-from functions.menu import menu
 import config as c
+from functions.menu import menu
 
 ### CSS AND STYLING
 
-st.logo("images/logome.png", icon_image = "images/logo_small.png")
+# Ladda och ändra storlek på logotypen
+logo_path = "images/logome.png"
+logo = Image.open(logo_path)
+logo = logo.resize((100, 100))  # Ändra storleken till 100x100 pixlar (justera efter behov)
+
+# Visa logotypen med den nya storleken
+st.image(logo, caption="Logotyp", use_column_width=False)
 
 page_config()
 styling()
@@ -34,7 +40,7 @@ with st.expander("### Övning"):  # Gör rubriken lika stor som underrubriken
     
     st.write("Tips. Klistra in texten först, gör sedan radbryt med hjälp av shift + enter och skriv in tre --- och efter det ytterligare ett radbryt med shift + enter. Skriv sedan din prompt.")
     
-     # Lägg till en bild
+    # Lägg till en bild
     st.image("images/me.png", caption="Lycka till!")
 
 # Check if language is already in session_state, else initialize it with a default value
@@ -73,47 +79,83 @@ if st.session_state["pwd_on"] == "true":
 
     if not check_password():
         st.stop()
-        
-### ### ### ###
+    
+############
+
+os.makedirs("data", exist_ok=True) # Where data such as documents are beeing stored
+
+
+if c.deployment == "streamlit":
+    llm = OpenAI(api_key = st.secrets.openai_key)
+    os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
+else:
+    llm = OpenAI(api_key = environ.get("openai_key"))
+    os.environ["OPENAI_API_KEY"] = environ.get("openai_key")
 
 
 # Translation
-
 if st.session_state['language'] == "Svenska":
-    chat_prompt = "Du är en hjälpsam AI-assistent. Svara på användarens frågor."
-    chat_clear_chat = "Rensa chat"
-    chat_hello = "Hej! Hur kan jag hjälpa dig?"
-    chat_settings = "Inställningar"
-    chat_choose_llm = "Välj språkmodell"
-    chat_choose_temp = "Temperatur"
-    chat_system_prompt = "Systemprompt"
-    chat_save = "Spara"
-    chat_imput_q = "Vad vill du prata om?"
+    prompt = """Du är en hjälpsam AI-assistent som hjälper användaren med sina frågor gällande den kontext du fått. Kontexten är ett eller flera dokument. 
+Basera alla dina svar på kontexten och hitta inte på något. Hjälp användaren svara på frågor, summera och annat. 
+Om du inte vet svaret, svarar du att du inte vet svaret.
+"""
+    clear_memory = "Rensa minnet"
+    cache_cleared = "Cachen och dina filer har rensats."
+    settings_text = "Inställningar"
+    temperature_text = "Temperatur"
+    system_prompt_text = "Systemprompt"
+    save_text = "Spara"
+    page_name = "Chatta med dina dokument"
+    loading_doc_text = "Laddar ditt dokument... Det här kan ta ett litet tag."
+    upload_file_text = "Ladda upp ett dokument för att starta chatten"
+    assistant_hello = "Hej! Hur kan jag hjälpa dig?"
+    chat_input_text = "Din fråga?"
+    thinking_text = "Jag tänker... Ett ögonblick..."
 
 elif st.session_state['language'] == "English":
-    chat_prompt = "You are a helpful AI assistant. Answer the user’s questions."
-    chat_clear_chat = "Clear chat"
-    chat_hello = "Hi! How can I help you?"
-    chat_settings = "Settings"
-    chat_choose_llm = "Choose language model"
-    chat_choose_temp = "Temperature"
-    chat_system_prompt = "System prompt"
-    chat_save = "Save"
-    chat_imput_q = "What do you want to talk about?"
-
-
-prompt = f"{chat_prompt}"
+    prompt = """You are a helpful AI assistant that helps the user with their questions regarding the context you have received. The context is one or more documents.  
+Base all your answers on the context and do not make anything up. Help the user answer questions, summarize, and other tasks. 
+If you don't know the answer, respond that you don't know the answer.
+"""
+    clear_memory = "Clear memory"
+    cache_cleared = "The cache and your files have been cleared."
+    settings_text = "Settings"
+    temperature_text = "Temperature"
+    system_prompt_text = "System prompt"
+    save_text = "Save"
+    page_name = "Chat with your documents"
+    loading_doc_text = "Loading your document... This may take a little while."
+    upload_file_text = "Upload a document to start the chat"
+    assistant_hello = "Hi! How can I help you?"
+    chat_input_text = "Your question?"
+    thinking_text = "I'm thinking... One moment..."
 
 
 if 'system_prompt' not in st.session_state:
     st.session_state.system_prompt = prompt
 
-
-# Check and set default values if not set in session_state
 if "llm_temperature" not in st.session_state:
-    st.session_state["llm_temperature"] = 0.7
-if "llm_chat_model" not in st.session_state:
-    st.session_state["llm_chat_model"] = "OpenAI GPT-4o"
+    st.session_state.llm_temperature = 0.2
+
+
+# Ensure each user has a unique directory
+if 'session_id' not in st.session_state:
+    st.session_state['session_id'] = str(uuid4())  # Generate a unique ID for each session
+
+# Create a user-specific data folder
+user_data_folder = f'./data/{st.session_state["session_id"]}'
+os.makedirs(user_data_folder, exist_ok=True)
+
+
+Settings.llm = OpenAI(
+    model="gpt-4o", 
+    temperature = st.session_state.llm_temperature,
+    system_prompt = st.session_state.system_prompt
+    )
+
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+embed_model = Settings.embed_model
+Settings.node_parser = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
 
 
 ### SIDEBAR
@@ -126,133 +168,142 @@ menu()
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button(f"{chat_clear_chat}", type="secondary"):
-        if "messages" in st.session_state.keys(): # Initialize the chat message history
-            st.session_state.messages = [
-                {"role": "assistant", "content": f"""
-                    {chat_hello}
-                 """}
-        ]
+        
+    if st.button(f"{clear_memory}"):
+        
+        # Delete all files in the 'data' folder
+        folder_path = user_data_folder
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)  # Remove the file
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)  # Remove the folder if necessary
+            except Exception as e:
+                st.error(f"Fel när filen {file_path} skulle tas bort: {e}")
 
+        # Clears all st.cache_resource caches:
+        st.cache_resource.clear()
+        st.success(f"{cache_cleared}")
+
+            
 with col2:
-    with st.expander(f"{chat_settings}"):
-        llm_model = st.selectbox(
-            f"{chat_choose_llm}", 
-            ["LLaMa3.1 8B", "LLaMa3 8B", "OpenAI GPT-4o", "OpenAI GPT-4o mini", "OpenAI o1-preview", "OpenAI o1-mini"],
-            index=["LLaMa3.1 8B", "LLaMa3 8B", "OpenAI GPT-4o", "OpenAI GPT-4o mini", "OpenAI o1-preview", "OpenAI o1-mini"].index(st.session_state["llm_chat_model"]),
-        )
+    with st.expander(f"{settings_text}"):
 
         llm_temp = st.slider(
-            f"{chat_choose_temp}",
+            f'{temperature_text}',
             min_value = 0.0,
             max_value = 1.0,
             step = 0.1,
-            value = st.session_state["llm_temperature"],
+            value = 0.1,
         )
-
-        # Update the session_state directly
-        st.session_state["llm_chat_model"] = llm_model
-        st.session_state["llm_temperature"] = llm_temp
-        
-        model_map = {
-            "OpenAI GPT-4o": "gpt-4o",
-            "OpenAI GPT-4o mini": "gpt-4o-mini",
-            "LLaMa3 8B": "llama3-8b-8192", 
-            "LLaMa3.1 8B": "llama-3.1-8b-instant",
-            "OpenAI o1-preview": "o1-preview", 
-            "OpenAI o1-mini": "o1-mini"
-        }
 
         st.markdown("###### ")
 
         with st.form("my_form"):
-            prompt_input = st.text_area(f"{chat_system_prompt}", prompt, height=200)
+            prompt_input = st.text_area(f"{system_prompt_text}", prompt, height=300)
             st.session_state.system_prompt = prompt_input   
-            st.form_submit_button(f"{chat_save}") 
+            st.form_submit_button(f'{save_text}') 
 
 
-if "OpenAI" in st.session_state["llm_chat_model"]:
-    st.sidebar.success("Språkmodell: " + llm_model)
-else:
-    st.sidebar.success("Språkmodell: " + llm_model)
+st.markdown(f"#### :material/description: {page_name}")
 
+@st.cache_resource(show_spinner=False)
+def load_data(user_data_folder):
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": f"{chat_hello}"}]
+    with st.spinner(text=f"{loading_doc_text}"):
 
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        # Check if the content is an image URL
-        if message["content"].startswith("http"):
-            st.image(message["content"])
-        else:
-            st.markdown(message["content"])
-
-
-# Define your system prompt here
-system_prompt = st.session_state.system_prompt
-
-if prompt := st.chat_input(f"{chat_imput_q}"):
-    
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-
-        # Preprocess messages to include the system prompt
-        processed_messages = []
-        for m in st.session_state.messages:
-            # Prepend system prompt to the user's message
-            if m["role"] == "user":
-                content_with_prompt = system_prompt + " " + m["content"]
-                processed_messages.append({"role": m["role"], "content": content_with_prompt})
-            else:
-                processed_messages.append(m)
-
-        if "OpenAI" in st.session_state["llm_chat_model"]:
-
-            if c.deployment == "streamlit":
-                client = OpenAI(api_key = st.secrets.openai_key)
-            else:
-                client = OpenAI(api_key = environ.get("openai_key"))
-    
-            for response in client.chat.completions.create(
-                model = model_map[st.session_state["llm_chat_model"]],
-                temperature = st.session_state["llm_temperature"],
-                messages = processed_messages,
-                stream = True,
-            ):
-                if response.choices[0].delta.content:
-                    full_response += str(response.choices[0].delta.content)
-                message_placeholder.markdown(full_response + "▌")  
-             
-        else:
-
-            if c.deployment == "streamlit":
-                client = Groq(api_key = st.secrets.groq_key)
-            else:
-                client = Groq(api_key = environ.get("groq_key"))
-
-            # Remove 'avatar' key for Groq messages
-            processed_messages_no_avatar = [{"role": m["role"], "content": m["content"]} for m in processed_messages]
-            stream = client.chat.completions.create(
-                messages=processed_messages_no_avatar,
-                model=model_map[st.session_state["llm_chat_model"]],
-                temperature=st.session_state["llm_temperature"],
-                max_tokens=1024,
-                top_p=1,
-                stop=None,
-                stream=True,
-            )
-
-            for chunk in stream:
-                if chunk.choices[0].delta.content is not None:
-                    full_response += str(chunk.choices[0].delta.content)
-                message_placeholder.markdown(full_response + "▌")
+        data = SimpleDirectoryReader(input_dir=user_data_folder, recursive=True).load_data()
                 
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        index = VectorStoreIndex.from_documents(
+            data, 
+            llm=llm,
+            embed_model=embed_model,
+            show_progress=True)
+        
+        return index    
+
+
+uploaded_files = st.file_uploader(
+    f"{upload_file_text}", 
+    type=("pdf", "docx", "doc", "xls", "xlsx", "csv"), 
+    accept_multiple_files=True)
+
+if uploaded_files:  # If files were uploaded
+
+    # Iterate through each uploaded file
+    for uploaded_file in uploaded_files:
+
+        file_path = f"{user_data_folder}/{uploaded_file.name}"
+
+        # Check if the new file is different from the already indexed files
+        if 'indexed_file_paths' not in st.session_state:
+            st.session_state.indexed_file_paths = []
+
+        if file_path not in st.session_state.indexed_file_paths:
+            st.session_state.indexed_file_paths.append(file_path)
+
+        # Save the file to disk
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+
+
+elif 'indexed_file_paths' in st.session_state:
+    # Reload existing files from the session state
+    #index = load_data()
+    pass
+
+
+if "messages" not in st.session_state.keys(): 
+    st.session_state.messages = [
+        {"role": "assistant", "content": f'''
+            {assistant_hello}
+        '''}
+    ]
+    st.session_state["messages"] = [{"role": "assistant", "content": f"{assistant_hello}"}]
+
+
+if uploaded_files:
+    index = load_data(user_data_folder)
+
+
+    query_engine = index.as_query_engine(
+        chat_mode = "openai", # openai context react
+        streaming = True,
+        similarity_top_k = 20,
+        verbose = True)
+
+
+    if prompt := st.chat_input(f"{chat_input_text}"): # Prompt for user input and save to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            # Check if the content is an image URL
+            if message["content"].startswith("http"):
+                st.image(message["content"])
+            else:
+                st.markdown(message["content"])
+
+
+    # If last message is not from assistant, generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            
+            full_response = ""
+            message_placeholder = st.empty()
+
+            with st.spinner(f"{thinking_text}"):
+                streaming_response = query_engine.query(prompt)
+                
+                for response in streaming_response.response_gen:
+                    full_response += response
+                    message_placeholder.markdown(full_response + "▌")
+                
+                message = {"role": "assistant", "content": full_response}
+        
+            message_placeholder.markdown(full_response)
+            
+        st.session_state.messages.append(message) # Add response to message history
