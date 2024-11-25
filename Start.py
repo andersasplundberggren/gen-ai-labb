d8ccd84658efd4abc488c1aba8985c1f28215089
andersasplundberggren <anders.asplundberggren@gmail.com>
2024-11-25 11:17:12 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -135,8 +135,6 @@ for title, content in sections:
             </div>
         """, unsafe_allow_html=True)
 
-st.write("I menyn till vänster kan du testa olika tjänster inom AI. Inget av det du gör här sparas, så om du får fram en bra text eller snygg bild får du se till att spara den.
-
-         Det här är en prototyp där information du matar in bearbetas med en språkmodell. Prototypen är inte GDPR-säkrad, då den använder AI-modeller som körs på servrar i USA.")
+st.write("I menyn till vänster kan du testa olika tjänster inom AI. Inget av det du gör här sparas, så om du får fram en bra text eller snygg bild får du se till att spara den. Tänk på att detta är en prototyp där information du matar in bearbetas med en språkmodell. Prototypen är inte GDPR-säkrad och använder AI-modeller som körs på servrar i USA.")
 
 st.write(" ")
