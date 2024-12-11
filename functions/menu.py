2284c7d8374b9b90e29b48f5c5d878c403bf5f60
andersasplundberggren <anders.asplundberggren@gmail.com>
2024-12-11 10:22:11 +0100

    Update menu.py

--- functions/menu.py
+++ functions/menu.py
@@ -42,7 +42,7 @@ def menu():
         st.sidebar.page_link("pages/image_analysis.py", label="Bildanalys", icon=":material/image:")
         st.sidebar.page_link("pages/chat_with_document.py", label="Chatta med dina dokument", icon=":material/description:")
         st.sidebar.page_link("pages/transcribe.py", label="Transkribering", icon=":material/transcribe:")
-        st.sidebar.page_link("pages/test.py", label="Dokumentera", icon=":material/forum:")
+        st.sidebar.page_link("pages/test.py", label="Dokumentera", icon=":edit_note/forum:")
         #ny sida
         # st.sidebar.page_link("pages/test.py", label="Test", icon=":material/labs:")
 
@@ -68,7 +68,7 @@ def menu():
         st.sidebar.page_link("pages/image_analysis.py", label="Image analysis", icon=":material/image:")
         st.sidebar.page_link("pages/chat_with_document.py", label="Chat with your documents", icon=":material/description:")
         st.sidebar.page_link("pages/transcribe.py", label="Transcribe", icon=":material/transcribe:")
-        st.sidebar.page_link("pages/test.py", label="Dokumentera", icon=":material/forum:")
+        st.sidebar.page_link("pages/test.py", label="Dokumentera", icon=":edit_note/forum:")
 
         st.sidebar.markdown("# ")
         with st.sidebar.container(border = True):
