1c931d99f973ebcab19baa196b6bda66b3073dcb
andersasplundberggren <anders.asplundberggren@gmail.com>
2025-01-20 10:25:49 +0100

    Update video.py

--- pages/video.py
+++ pages/video.py
@@ -1,16 +1,21 @@
+import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Skapa några testdata
+# Testdata
 data = {
     'År': [2024, 2025, 2026, 2027, 2028],
     'Befolkning': [1724, 1702, 1690, 1680, 1670]
 }
+
 df = pd.DataFrame(data)
 
-# Skapa en enkel linjediagram
-plt.plot(df['År'], df['Befolkning'])
-plt.title('Testdiagram')
-plt.xlabel('År')
-plt.ylabel('Befolkning')
-plt.show()
+# Skapa diagram
+fig, ax = plt.subplots()
+ax.plot(df['År'], df['Befolkning'])
+ax.set_title('Testdiagram')
+ax.set_xlabel('År')
+ax.set_ylabel('Befolkning')
+
+# Visa diagram i Streamlit
+st.pyplot(fig)
